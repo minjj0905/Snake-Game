2a33c0dda1c1c28b2d61df4dd29470d59b51a3a7
Eun-Su-Kim <godong9985@gmail.com>
2020-06-11 02:39:15 +0900

    아이템 구현

--- src/Item.cpp
+++ src/Item.cpp
@@ -1,1 +1,59 @@
-#include 
+#include <iostream>
+#include "Item.h"
+using namespace std;
+
+POSITION::POSITION(int row, int col) {
+    x = col;
+    y = row;
+}
+
+POSITION::POSITION() {
+    x = 0;
+    y = 0;
+}
+
+Item::Item() {
+    int itemCount = 0;
+    string itemType = "";
+}
+
+void Item::randomItem(Map& m) {
+    srand((unsigned int) time(0));
+    if ((m.map[rand() % m.mapHeight][rand() % m.mapWidth] == 0) && (itemCount < 3)) {
+        if (rand() % 2 == 0) {
+            growItem(rand() % m.mapHeight, rand() % m.mapWidth);
+            itemCount++;
+        } else {
+            poisonItem(rand() % m.mapHeight, rand() % m.mapWidth);
+            itemCount++;
+        }
+    }
+}
+
+void Item::growItem(int y, int x) {
+    growItemPos.push_back(POSITION(y, x));
+    itemType = "grow";
+}
+
+void Item::poisonItem(int y, int x) {
+    poisonItemPos.push_back(POSITION(y, x));
+    itemType = "poison";
+}
+
+POSITION Item::getItemPos() {
+    if (itemType == "grow" && !growItemPos.empty()) {
+        int xpos = growItemPos[0].x;
+        int ypos = growItemPos[0].y;
+        growItemPos.clear();
+        return POSITION(ypos, xpos);
+    } else if (itemType == "poison" && !poisonItemPos.empty()) {
+        int xpos = poisonItemPos[0].x;
+        int ypos = poisonItemPos[0].y;
+        poisonItemPos.clear();
+        return POSITION(ypos, xpos);
+    } else {
+        return POSITION();
+    }
+}
+
+
--- src/Item.h
+++ src/Item.h
@@ -3,25 +3,42 @@
 
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
+#include <vector>
 #include "Map.h"
+
+struct POSITION {
+    int x, y;
+    POSITION(int y, int x);
+    POSITION();
+};
 
 class Item {
 public :
+
+    Item();
     
     /*맵에 나와있는 아이템의 전체 갯수*/
-    int itemCount = 0;
+    int itemCount;
 
-    char poison;
+    /*아이템의 타입 grow, poison*/
+    string itemType;
 
-    char grow;
+    /*grow 아이템의 정보를 갖게한다.*/
+    void growItem(int y, int x);
 
-    void poisonItem();
+    /*poison 아이템의 정보를 갖게한다.*/
+    void poisonItem(int y, int x);
 
-    void growItem();
-
+    /*랜덤으로 아이템 생성*/
     void randomItem(Map& m);
 
+    /*각 아이템들의 좌표*/
+    vector<POSITION> growItemPos;
+    vector<POSITION> poisonItemPos;
 
+    /*각 아이템들의 좌표값을 리턴*/
+    POSITION getItemPos();
 
 };
 #endif
