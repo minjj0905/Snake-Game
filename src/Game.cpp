444992f016517c0f81989bb3a3ecbaebd1428a6a
minjj0905 <kmj2967@kookmin.ac.kr>
2020-06-12 15:29:14 +0900

    Fix run level
    - Snake 이동방식 개선에 따른 수정

--- src/Game.cpp
+++ src/Game.cpp
@@ -34,6 +34,7 @@ void Game::runLevel() {
     while(!isGameOver()) {
         timer.updateTime();
         double a = timer.getTick();
+        curSnake.setDirection();
         if(a > 0.5) {
             curSnake.moveSnake();
             view.draw(curMap, curSnake);
@@ -54,9 +55,6 @@ bool Game::isGameOver() {
 bool Game::isCollision() {
     POSITION headpos = curSnake.getPosition()[0];
     int posvalue = curMap.getMapValue(headpos.y, headpos.x);
-    // mvprintw(20, 90, std::to_string(posvalue).c_str());
-    // mvprintw(21, 90, std::to_string(headpos.y).c_str());
-    // mvprintw(22, 90, std::to_string(headpos.x).c_str());
     if((posvalue == 1) || (posvalue == 2)) return true;
 
     return false;
